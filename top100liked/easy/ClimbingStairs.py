2a282d5beb11bc9bd6291008d099c10e2c6223e1
zqbinggong <2420086330@qq.com>
2018-05-10 23:37:55 +0800

    top100liked

--- top100liked/easy/ClimbingStairs.py
+++ top100liked/easy/ClimbingStairs.py
@@ -6,7 +6,9 @@ class Solution:
         """
         for n in range(3):
             return n
-        a = 1, b = 2, i = 3
+        a = 1
+        b = 2
+        i = 3
         while i <= n:
             a, b = b, a + b
             i += 1
